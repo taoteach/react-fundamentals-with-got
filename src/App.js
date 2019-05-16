4ffe71cbef29e1b6ba565f2c5c4f0eacb017a5d3
Veuge <nicky.0592@gmail.com>
2019-05-15 23:04:33 -0400

    Modified from function component to class component

--- src/App.js
+++ src/App.js
@@ -10,37 +10,39 @@ import Person from './components/Person';
  * }
  */
 
-function App() {
-   const tyrion = {
-      id: 1,
-      name: 'Tyrion Lannister',
-      isDead: false
-   };
-   const jon = {
-      id: 2,
-      name: 'Jon Snow',
-      isDead: false
-   };
-   const dany = {
-      id: 3,
-      name: 'Daenerys Targaryen',
-      isDead: false
-   };
-
-   return (
-      <div>
-         <header>
-            <h1>GOT, WHO WILL DIE?</h1>
-         </header>
-         <main>
-            <div className="list-container">
-               <Person data={tyrion} />
-               <Person data={jon} />
-               <Person data={dany} />
-            </div>
-         </main>
-      </div>
-   );
+class App extends React.Component {
+   render() {
+      const tyrion = {
+         id: 1,
+         name: 'Tyrion Lannister',
+         isDead: false
+      };
+      const jon = {
+         id: 2,
+         name: 'Jon Snow',
+         isDead: false
+      };
+      const dany = {
+         id: 3,
+         name: 'Daenerys Targaryen',
+         isDead: false
+      };
+      
+      return (
+         <div>
+            <header>
+               <h1>GOT, WHO WILL DIE?</h1>
+            </header>
+            <main>
+               <div className="list-container">
+                  <Person data={tyrion} />
+                  <Person data={jon} />
+                  <Person data={dany} />
+               </div>
+            </main>
+         </div>
+      );
+   }
 }
 
 export default App;
