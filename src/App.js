485927f006e037054921e26d23219180d33b5d11
Veuge <nicky.0592@gmail.com>
2019-05-15 22:59:18 -0400

    Component with props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React from 'react';
 import Person from './components/Person';
 
+/**
+ * Person
+ * {
+ *    id: 1,
+ *    name: 'Tyrion Lannister',
+ *    isDead: true | false
+ * }
+ */
+
 function App() {
+   const tyrion = {
+      id: 1,
+      name: 'Tyrion Lannister',
+      isDead: false
+   };
+   const jon = {
+      id: 2,
+      name: 'Jon Snow',
+      isDead: false
+   };
+   const dany = {
+      id: 3,
+      name: 'Daenerys Targaryen',
+      isDead: false
+   };
+
    return (
       <div>
          <header>
@@ -9,9 +34,9 @@ function App() {
          </header>
          <main>
             <div className="list-container">
-               <Person />
-               <Person />
-               <Person />
+               <Person data={tyrion} />
+               <Person data={jon} />
+               <Person data={dany} />
             </div>
          </main>
       </div>
